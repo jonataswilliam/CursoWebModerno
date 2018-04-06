0fbc65ee9eafbe1eeb510d607151e805aa2fce47
Jonatas William <jonatas_2602@hotmail.com>
2018-04-05 22:33:01 -0300

    Primeiro Commit

---
+++ fundamentos/comentario.js
@@ -0,0 +1,8 @@
+// Comentario de uma linha
+console.log ('Linha 1')
+
+/*
+ * Comentario de
+ * multiplas linhas
+*/
+
---
+++ fundamentos/organizacao.js
@@ -0,0 +1,11 @@
+console.log('Sentença de código')
+
+{
+    // Bloco de codigo. Serve para agrupar codigo
+    {
+        console.log("Ola")     // Sentença 1   
+        console.log('Mundo')   // Sentença 2
+    }
+}
+
+// JS é estruturado e organizado por Sentencas e blocos de código podendo ou nao terminar com ;
