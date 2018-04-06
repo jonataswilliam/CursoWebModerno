1fbe1e025cd3ac04c5fe8d55fbb4f23335baa19b
Jonatas William <jonatas_2602@hotmail.com>
2018-04-06 15:02:16 -0300

    Fim da aula 6 - thisEBind - funcoes

--- funcao/thisPodeVariar.js
+++ funcao/thisPodeVariar.js
@@ -10,4 +10,11 @@ function f1();
 O RETORNO SERÀ TRUE. POIS O CONTEXTO EM QUE A FUNCAO FOI CHAMADA FOI GLOBAL (WINDOW) NO NAVEGADOR.
 
 SE A FUNCAO FOR ACIONADO COM UM CLICK POR EXEMPLO, THIS NÃO SERÁ MAIS === A WINDOW POIS A CHAMADA FOI FEITA PELO ELEMENTO QUE FOI CLICADO.
+
+Quando temos uma funcao tradicional em JS, this pode variar de acordo com quem chamou a funcao.
+
+
+Dois grandes motivos para a crição da ArrowFunction
+1. A Sintaxe reduzida
+2. This não varia. This sempre será definido no contexto em que a funcao foi definida. Também conhecida como THIS LEXICO, pois é definido no contexto em que foi excrito.
 */
