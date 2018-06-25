903c08e9a6441c4bf18277eecdbde8a8e2164940
Jonatas William <jonatas_2602@hotmail.com>
2018-06-25 14:40:47 -0300

    Fim Agendamento de tarefas

---
+++ node/temporizador.js
@@ -0,0 +1,22 @@
+const schedule = require('node-schedule')
+
+// String de acordo com os parametros passadossignifica que será executada de(5 em 5s(*/. Se colocarmos apenas o numero ele será executado apenas naquele minuto), em qualquer minuto(*), as 14 horas, em qualquer dia do mes(*), em qualquer mes(*), executar na segunda feira(1. A contagem comeca sendo domingo = 0)
+const tarefa1 = schedule.scheduleJob('*/5 * 14 * * 1', function () {
+    console.log('Executando Tarefa 1', new Date().getSeconds())
+})
+
+setTimeout(function () {
+    tarefa1.cancel()
+    console.log('Tarefa 1 cancelada!')
+}, 20000)
+
+
+// Agendamento de tarefa de outra maneira, utilizando o schedule.RecurrenceRule()
+const regra = new schedule.RecurrenceRule()
+regra.dayOfWeek = [new schedule.Range(1,5)]
+regra.hour = 14
+regra.second = 10
+
+const tarefa2 = schedule.scheduleJob(regra, function () {
+    console.log('Executando Tarefa 2!', new Date().getSeconds())
+})
